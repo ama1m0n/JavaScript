1936ed787a2eaa08d72d3c5bc0d5fb4336b032f1
ama1m0n <bbadlok@gmail.com>
2023-01-26 14:21:05 +0300

    HW1

---
+++ 24-01HW/script.js
@@ -0,0 +1,23 @@
+let num = 10;
+num = --num;
+num = num++;
+num = ++num;
+console.log(num)
+
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
+
+
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
