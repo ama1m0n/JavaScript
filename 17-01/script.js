b3db8d495688e7a2505e7908492bb4e3bee597b2
ama1m0n <bbadlok@gmail.com>
2023-01-26 13:22:16 +0300

    first commit

---
+++ 17-01/script.js
@@ -0,0 +1,53 @@
+/******* Number ******/
+
+/*let age = 45;
+let price = 10.5;*/
+
+//infinity, NaN
+
+/*
+let result = 58 / 0;
+alert(result);*/
+
+
+/*let x = 10;
+let y = "javascript";
+let result = y * x;
+alert(result);*/
+
+/*
+let name = 'vlad';
+let surname = "pyatkov";
+let lastName = `sergeevich`;
+let fio = `Фио пользователя: ${surname} ${name} ${lastName}`;
+
+alert(fio);*/
+
+/***** Boolean ******/
+/*
+let isProgrammer = true;
+let disabled = false;
+let result = 50 < 60;
+alert(result);*/
+
+/***** NULL *****/
+/*
+let name = null;
+alert(name);
+*/
+
+/********** Undefined ***********/
+/*
+let country;
+alert(country);*/
+/******** Object *******/
+/*
+let car = {
+    color:"red",
+    name:"BMW"
+}
+console.log(car);*/
+
+/********* Bigint ***********/
+let num = 1212121212121212565n;
+console.log(num);
---
+++ 24-01/main.js
@@ -0,0 +1,62 @@
+/********* Вычитание с последующим присвоением результата **********/
+
+/*let age = 18;
+age -= 5; //age = age - 5
+console.log(age);*/
+
+/********** Умножение с последующим присвоением результата ********/
+/*
+let price = 500;
+price *= 5; //price = price * 5;
+console.log(price);*/
+/************ Деление с последующим присвоением результа **************/
+/*
+let count = 50;
+count /= 5; //count = count / 5
+console.log(count);*/
+/************ Оператор равенства ****************/
+/*
+let password1 = '123';
+let password2 = 123;
+let result = password1 == password2;
+console.log(result);
+console.log(typeof password1);
+console.log(typeof password2);*/
+/********** Строгое равенство ***********/
+/*let name = 'Vlad';
+let age = 'Vlad1';
+let result = name === age;
+console.log(result);*/
+/********* != ********/
+/*let user = 'test';
+let new_user = 'player';
+let result = user != 'test';
+console.log(result); //false*/
+/******** != ********/
+/*let user_role = 4;
+let result = user_role !== '4';
+console.log(result);*/
+/******** > *******/
+/*
+let result = 5 > 6;
+console.log(result);
+*/
+/********* < ********/
+/*let age = 22;
+let control = age < 24;
+console.log(control);*/
+/******** >= *********/
+/*let age = 16;
+let control_age = 18;
+let result = age >= control_age;
+console.log(result);*/
+/********** & **********/
+/*let age = 40;
+let result =age > 30 && age < 40;
+console.log(result);*/
+/******** || или *******/
+let age = 18;
+let result = age > 18 || age < 24;
+console.log(result);
+
+
