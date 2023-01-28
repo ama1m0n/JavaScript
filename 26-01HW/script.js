991cb4605b5ed105d2a390cc17e2c51da95d5ee5
ama1m0n <bbadlok@gmail.com>
2023-01-28 12:59:10 +0300

    26-01homework

---
+++ 26-01HW/script.js
@@ -0,0 +1,31 @@
+/*let x = 7;
+if (x == 7) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}*/
+
+/*
+let a = 1;
+let b = 3;
+let pos_summ = a + b;
+let neg_summ = a - b;
+if (a == 1 || a < 1 && b > 3 || b == 3) {
+    alert(pos_summ);
+} else {
+    alert(neg_summ);
+};*/
+
+let time = 27;
+if (time >= 0 && time <= 14) {
+    alert('В первую четверть.');
+}
+if (time >= 15 && time <= 30) {
+    alert('Во вторую четверть.');
+}
+if (time >= 31 && time <= 45) {
+    alert('В третью четверть.');
+}
+if (time >= 46 && time <= 59) {
+    alert('В четвертую четверть.');
+}
